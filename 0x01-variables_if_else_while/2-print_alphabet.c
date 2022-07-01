98a4f776eed73bcd536c79e93b32801c05130a31
Wambu Scola <scola@github.com>
2022-07-01 04:33:30 -0700

    alphabet low

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ *main - prints the alphabet in lowercase
+ *Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char alph = 'a';
+
+	while (alph <= 'z')
+	{
+		putchar(alph);
+		alph++;
+	}
+	putchar('\n');
+	return (0);
+}
