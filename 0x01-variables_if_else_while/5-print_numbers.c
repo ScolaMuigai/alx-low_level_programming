d4708733e3460bc9a2543cc335ce28bfbddc7a17
Wambu Scola <scola@github.com>
2022-07-01 06:10:32 -0700

    integers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ *main - print integers 0 to 9
+ *Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+int n = 0;
+
+for (n < 10; n++)
+{
+	printf("%i", n);
+}
+putchar('\n');
+
+return (0);
+}
