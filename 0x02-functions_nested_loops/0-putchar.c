c61309cb6ef733f22ce09997bed4df0434999fcf
Wambu Scola <scola@github.com>
2022-07-06 03:07:16 -0700

    putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,4 +1,4 @@
-#include <stdio.h>
+#include "main.h"
 
 /**
 * _putchar prints the name _putchar
@@ -6,6 +6,15 @@
 
 int main(void)
 {
-puts ("_putchar");
+_putchar('_');
+_putchar('p');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_putchar('a');
+_putchar('r');
+_putchar('\n');
+
 return (0);
 }
