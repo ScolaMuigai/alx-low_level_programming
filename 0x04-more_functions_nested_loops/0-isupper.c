272685dcb3e9112d0431cab673e40f18f860869d
Wambu Scola <scola@github.com>
2022-07-07 03:15:27 -0700

    uppercase

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -2,10 +2,9 @@
 #include <stdio.h>
 
 /**
-* The _isupper() checks if the character is uppercase
+* The _isupper checks if the character is uppercase
 * @c: character to be checked
-* Retun 1 if c is uppercase
-*Return o otherwise
+* Retun; 1 if true, o if false
 */
 
 int _isupper(int c)
@@ -14,8 +13,6 @@ int _isupper(int c)
 	{
 	return (1);
 	}
-	else
-	{
+
 	return (0);
-	}
 }
